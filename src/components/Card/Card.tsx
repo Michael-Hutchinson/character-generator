9d700330fe8de57e2cdcb6df0840c092300bf198
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-02-17 11:25:32 +0000

    create card component

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Wrapper } from './Card.styles';
+
+const Card: React.FunctionComponent = () => (
+  <Wrapper>
+    <p>Card Component</p>
+  </Wrapper>
+);
+
+export default Card;

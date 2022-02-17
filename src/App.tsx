c2bd39ece79407149ab9b095143ba7e353e1ac7d
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-02-17 15:10:20 +0000

    start building api states

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import { Wrapper } from 'App.styles';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Card from './components/Card/Card';
 
 const App: React.FunctionComponent = () => {
+  const [character, setCharacter] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [characterId, setCharacterId] = useState(1);
+
+  useEffect(() => {
+    const fetchFromApi = async () => {
+      setIsLoading(true);
+    };
+  }, [characterId]);
+
   return (
     <Wrapper>
       <Card name="Michael" imgUrl="tbc" />

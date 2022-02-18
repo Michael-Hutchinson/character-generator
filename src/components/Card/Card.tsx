dcf57a2ecd09b2a899b9145b218cce1e0f79bbf5
Michael Hutchinson <michael-hutchinson@hotmail.co.uk>
2022-02-18 18:08:07 +0000

    add gender to character card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 
+import { StyledGender } from '../Gender/Gender.styles';
 import { Wrapper } from './Card.styles';
 
 interface ICardProps {
   name: string;
   imgUrl: string;
+  gender: string;
 }
 
-const Card: React.FunctionComponent<ICardProps> = ({ name, imgUrl }) => (
+const Card: React.FunctionComponent<ICardProps> = ({ name, imgUrl, gender }) => (
   <Wrapper>
     <p>{name}</p>
     <img src={imgUrl} alt={name} />
+    <StyledGender color="green" gender={gender} />
   </Wrapper>
 );
 
